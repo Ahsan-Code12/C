859be19c87f8b00b1468ebebd41ed40a518e5583
AhsanCodes1 <khawar1935@gmail.com>
2026-09-02 09:58:46 +0500

    upadte

--- code-snippet/Hello.c
+++ code-snippet/Hello.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 
-int func(int param1, int param2){
-    for(int i = 0; i <= param1 + param2;i++){
-        printf("%d ",i * param1 - param2);
-    }
-}
-
 int main() {
-
-    int parameter1 = 5;
-    int parameter2 = 3;
-
-    
-    printf("hello, World!\n");
-    func(parameter1, parameter2);
+    printf("hello\n");
     return 0;
 }
