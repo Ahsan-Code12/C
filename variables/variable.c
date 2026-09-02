257b979296cbd5e30f6867668d3aeb68963b1f8a
AhsanCodes1 <khawar1935@gmail.com>
2026-09-02 10:52:44 +0500

    update

--- variables/variable.c
+++ variables/variable.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int _first = 10; // This is a variable declaration and initialization
-    int _b2 = 20; // Another variable declaration and initialization
-    char ch = 'A'; // A character variable
+    int var;
+    scanf("%d", &var);
+
+    printf("%d\n", var);
+
+    return 0;
 }
