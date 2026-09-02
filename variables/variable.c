95938161a52669c74b46d5df3ace9d8679d91c4c
AhsanCodes1 <khawar1935@gmail.com>
2026-09-02 10:35:45 +0500

    update

---
+++ variables/variable.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10; // This is a variable declaration and initialization
+    int b = 20; // Another variable declaration and initialization
+    char ch = 'A'; // A character variable
+}
