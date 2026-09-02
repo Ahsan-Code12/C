c8b7c8f39d99323555b643e84aa917f1c726d7c6
AhsanCodes1 <khawar1935@gmail.com>
2026-09-02 11:27:47 +0500

    update

--- variables/Project.c
+++ variables/Project.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 
-int main() {
-    /*
-    simple program knowing some c++ in c
-    */
+// Function prototypes taake compiler ko pehle se pata ho
+void InvestMent_Tracker();
+void Algo();
 
+void Algo()
+{
+    int user_12;
+    printf("Wanna track your investments? Let's get started!\n");
+    printf("Enter 2 to Start, 1 to Exit: ");
+    scanf("%d", &user_12); // %d use kiya taake direct integer match ho
+
+    if (user_12 == 2)
+    {
+        InvestMent_Tracker();
+    }
+    else if (user_12 == 1)
+    {
+        printf("Exiting the program.\n");
+    }
+    else
+    {
+        printf("Invalid input. Please enter 1 or 2.\n");
+        Algo();
+    }
+}
+
+void InvestMent_Tracker()
+{
     int Investment;
     printf("Enter the investment amount: ");
     scanf("%d", &Investment);
@@ -14,14 +37,14 @@ int main() {
     scanf("%d", &Years_For_Investment);
 
     int Return_Rate;
-    printf("Enter the return rate (as a percentage): ");
+    printf("Enter the return rate (as a percentage, e.g., 100 for double): ");
     scanf("%d", &Return_Rate);
 
     int Curent_Ammount = Investment;
-
     int i = 1;
-    
-    while (i <= Years_For_Investment) {
+
+    while (i <= Years_For_Investment)
+    {
         Curent_Ammount = Curent_Ammount * Return_Rate / 100 + Curent_Ammount;
         printf("Year %d: %d\n", i, Curent_Ammount);
         i++;
@@ -31,11 +54,19 @@ int main() {
     printf("Enter 1 to quit or 0 to continue: ");
     scanf("%d", &Quit_Not);
 
-    if(Quit_Not == 1) {
+    if (Quit_Not == 1)
+    {
         printf("Exiting the program.\n");
-    } else {
-        printf("Continuing the program.\n");
-        main();
     }
+    else
+    {
+        printf("Continuing the program.\n");
+        Algo(); // Dobara menu par le jane ke liye
+    }
+}
 
+int main()
+{
+    Algo();
+    return 0;
 }
